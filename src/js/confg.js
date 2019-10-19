c8d6d358ae896d84eb9dbfd12db50a3faabb1d28
vijay yadav <vizzyadav1997@gmail.com>
2019-10-19 17:34:20 +0530

    first commit

---
+++ src/js/confg.js
@@ -0,0 +1,7 @@
+export const key = 'f0477f2c28270ed6e8e25889daee94bb';
+
+
+//gmail => 83538e0e33f64de57cc1ea6c5399e3aa
+//outlook => af2e98216fb97f0348a9318f8742497c 
+//outlook => 696f42bf0917aaa5bd8908b8ab0462e1
+//vandana => f0477f2c28270ed6e8e25889daee94bb
